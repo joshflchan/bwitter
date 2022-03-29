9072ffa2378a2692b941b8ef9482d2ad08a35606
UBC Student <ianmah@students.cs.ubc.ca>
2022-03-29 15:52:39 -0700

    bweethlib.Start()

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"cs.ubc.ca/cpsc416/p2/bwitter/bweethlib"
+)
+
 func main() {
 	// Code for client to run, prolly smth smth read from config smth smth tweet this smth smth see all tweets
+
+	client := bweethlib.NewBweeth()
+	notifCh, err := client.Start("0x8008153", "127.0.0.1:1234", 3)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for i := 0; i < 3; i++ {
+		result := <-notifCh
+		log.Println("NOTIFYCH RESULT:", result)
+	}
 }
