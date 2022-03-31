758a0ce15d8ae3662bb8c8db3c8a6dd53972bfc0
Adin Kwok <adin@adin.dev>
2022-03-30 18:12:54 -0700

    [COORD] Implement GetPeers()
    Signed-off-by: Adin Kwok <adin@adin.dev>

--- bwitter/coord.go
+++ bwitter/coord.go
@@ -2,20 +2,93 @@ package bwitter
 
 import (
 	"fmt"
+	"math/rand"
+	"net"
 	"net/rpc"
+	"reflect"
+	"time"
 )
 
 type Coord struct {
+	MinerPool map[string]string
+
+	MinNumNeighbors    int16
+	CoordListenAddress string
 }
 
-func (c *Coord) Start() {
+func NewCoord() *Coord {
+	return &Coord{}
+}
+
+func (c *Coord) Start(CoordListenAddress string, MinNumNeighbors int16) error {
+	rand.Seed(time.Now().UTC().UnixNano())
+	c.CoordListenAddress = CoordListenAddress
+	c.MinNumNeighbors = MinNumNeighbors
+
 	err := rpc.Register(c)
 	if err != nil {
 		fmt.Println("failed to register")
+		return err
 	}
+	coordListener, err := net.Listen("tcp", c.CoordListenAddress)
+	if err != nil {
+		return err
+	}
+	go rpc.Accept(coordListener)
+
+	return nil
 }
 
-// TODO: do we only want to return x number of servers?
-func (c *Coord) GetPeers() {
+// Coord.GetPeers
+type CoordGetPeersArgs struct {
+	IncomingMinerAddr string
+}
+type CoordGetPeersResponse struct {
+	NeighborAddrs []string
+}
 
+func (c *Coord) GetPeers(args *CoordGetPeersArgs, response *CoordGetPeersResponse) error {
+	fmt.Println(args.IncomingMinerAddr + ": Coord.GetPeers received")
+	addrsToReturn := make(map[string]bool)
+	keys := reflect.ValueOf(c.MinerPool).MapKeys()
+	for i := int16(0); i < c.MinNumNeighbors && i < int16(len(c.MinerPool)); i++ {
+		randomIndex := rand.Intn(len(c.MinerPool))
+		randomMinerAddr := keys[randomIndex].Interface().(string)
+
+		if randomMinerAddr == args.IncomingMinerAddr {
+			fmt.Println(args.IncomingMinerAddr + ": randomMinerAddr is same as requester")
+			continue
+		} else if addrsToReturn[randomMinerAddr] {
+			fmt.Println(args.IncomingMinerAddr + ": randomMinerAddr has already been selected")
+			continue
+		}
+		addrsToReturn[randomMinerAddr] = true
+	}
+	response.NeighborAddrs = make([]string, 0, len(addrsToReturn))
+	for key := range addrsToReturn {
+		response.NeighborAddrs = append(response.NeighborAddrs, key)
+	}
+	return nil
+}
+
+// Coord.NotifyJoin
+type CoordNotifyJoinArgs struct {
+	IncomingMinerAddr string
+	MinerFcheckAddr   string
+}
+type CoordNotifyJoinResponse struct {
+}
+
+func (c *Coord) NotifyJoin(args *CoordNotifyJoinArgs, response *CoordNotifyJoinResponse) error {
+	fmt.Println(args.IncomingMinerAddr + ": Coord.NotifyJoin received")
+
+	c.MinerPool[args.IncomingMinerAddr] = args.MinerFcheckAddr
+
+	go fcheckMiner(c, args.IncomingMinerAddr, args.MinerFcheckAddr)
+	return nil
+}
+
+func fcheckMiner(c *Coord, minerListenAddr string, minerFcheckAddr string) {
+	fmt.Println(minerListenAddr + ": beginning fcheck to " + minerFcheckAddr)
+	// TODO
 }
--- cmd/coord/main.go
+++ cmd/coord/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"cs.ubc.ca/cpsc416/p2/bwitter/bwitter"
+)
+
+type CoordConfig struct {
+	CoordListenAddress string
+	MinNumNeighbors    int16
+}
+
 func main() {
-	// Code for coord to start, prolly smth smth read from config smth smth server.Accept()
+	coordConfig := ReadConfig("../config/coord_config.json")
+
+	coord := bwitter.NewCoord()
+
+	err := coord.Start(coordConfig.CoordListenAddress, coordConfig.MinNumNeighbors)
+	CheckErr(err, "unable to start coord")
+}
+
+func ReadConfig(filepath string) *CoordConfig {
+	configFile := filepath
+	configData, err := ioutil.ReadFile(configFile)
+	CheckErr(err, "reading coord config file")
+
+	config := new(CoordConfig)
+	err = json.Unmarshal(configData, config)
+	CheckErr(err, "parsing coord config data")
+
+	return config
+}
+
+func CheckErr(err error, errfmsg string, fargs ...interface{}) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, errfmsg, fargs...)
+		os.Exit(1)
+	}
 }
