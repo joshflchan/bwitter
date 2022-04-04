8e48e38a5918fa3a8d8ee055aef46bd584fcb515
UBC Student <huangsa@thetis.students.cs.ubc.ca>
2022-04-04 15:08:07 -0700

    Propagation, untested

--- bwitter/miner.go
+++ bwitter/miner.go
@@ -39,6 +39,11 @@ type Miner struct {
 	TransactionsList []Transaction
 }
 
+type Transaction struct {
+	Timestamp string
+	Tweet     string
+}
+
 type MiningBlock struct {
 	SequenceNum  int
 	MinerID      string
@@ -48,9 +53,11 @@ type MiningBlock struct {
 	CurrentHash  string
 }
 
-type Transaction struct {
-	Timestamp string
-	Tweet     string
+type PropagateArgs struct {
+	Block MiningBlock
+}
+
+type PropagateResponse struct {
 }
 
 func NewMiner() *Miner {
@@ -201,6 +208,7 @@ func (m *Miner) addNewMinerToPeersList(newRequestedPeers []string) {
 
 // RPC Call for client
 func (m *Miner) Post(postArgs *util.PostArgs, response *util.PostResponse) error {
+	// TODO: somewhere in here we should check if operation is seen already
 	log.Println("POST msg received:", postArgs.MessageContents)
 	msgContent := postArgs.MessageContents + postArgs.Timestamp
 
@@ -232,6 +240,11 @@ func (m *Miner) Post(postArgs *util.PostArgs, response *util.PostResponse) error
 
 	log.Println("tx:", transaction)
 	// propagate op [JOSH]
+
+	var reply util.PostResponse
+	for _, peerConnection := range m.PeersList {
+		peerConnection.Call("Miner.Post", postArgs, &reply)
+	}
 
 	return nil
 }
@@ -373,6 +386,16 @@ func (m *Miner) broadcastMinedBlock() {
 			// 	continue
 			// }
 		}
+	}
+}
+
+func (m *Miner) PropagateBlock(propagateArgs *PropagateArgs, response *PropagateResponse) {
+	// Validate the block
+
+	// Propagate to peers
+	var reply PropagateResponse
+	for _, peerConnection := range m.PeersList {
+		peerConnection.Call("Miner.PropagateBlock", propagateArgs, &reply)
 	}
 }
 
